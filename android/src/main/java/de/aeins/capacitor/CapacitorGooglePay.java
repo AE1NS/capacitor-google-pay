182457cb5c1b29c9c2f3337ec3133067bba49230
Jan Gessinger <jgessinger@aeins.de>
2021-01-15 15:10:12 +0100

    - removed failure handler before UI shows (onActivityResults handles this) - added requestCodes to NativePlugin to trigger handleOnActivityResult action

--- android/src/main/java/de/aeins/capacitor/CapacitorGooglePay.java
+++ android/src/main/java/de/aeins/capacitor/CapacitorGooglePay.java
@@ -10,24 +10,24 @@ import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
-
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.wallet.AutoResolveHelper;
+import com.google.android.gms.wallet.IsReadyToPayRequest;
 import com.google.android.gms.wallet.PaymentData;
 import com.google.android.gms.wallet.PaymentDataRequest;
 import com.google.android.gms.wallet.PaymentsClient;
 import com.google.android.gms.wallet.Wallet;
 import com.google.android.gms.wallet.WalletConstants;
-import com.google.android.gms.wallet.IsReadyToPayRequest;
 
 import org.json.JSONException;
 
-@NativePlugin
+@NativePlugin(requestCodes = {CapacitorGooglePay.LOAD_PAYMENT_DATA_REQUEST_CODE})
 public class CapacitorGooglePay extends Plugin {
-    private static final int LOAD_PAYMENT_DATA_REQUEST_CODE = 42;
+
+    static final int LOAD_PAYMENT_DATA_REQUEST_CODE = 42;
 
     private PaymentsClient paymentsClient = null;
 
@@ -35,38 +35,34 @@ public class CapacitorGooglePay extends Plugin {
     protected void handleOnActivityResult(int requestCode, int resultCode, Intent data) {
         super.handleOnActivityResult(requestCode, resultCode, data);
 
-        switch (requestCode) {
-            case LOAD_PAYMENT_DATA_REQUEST_CODE:
-                PluginCall call = this.getSavedCall();
-                switch (resultCode) {
-                    case Activity.RESULT_OK:
-                        PaymentData paymentData = PaymentData.getFromIntent(data);
-                        try {
-                            call.success(new JSObject(paymentData.toJson()));
-                        } catch (JSONException e) {
-                            call.error(e.getMessage(), e);
-                        }
-                        break;
-                    case Activity.RESULT_CANCELED:
-                        call.error("Payment cancelled");
-                        break;
-                    case AutoResolveHelper.RESULT_ERROR:
-                        Status status = AutoResolveHelper.getStatusFromIntent(data);
+        PluginCall call = this.getSavedCall();
+        switch (resultCode) {
+            case Activity.RESULT_OK:
+                PaymentData paymentData = PaymentData.getFromIntent(data);
+                try {
+                    call.success(new JSObject(paymentData.toJson()));
+                } catch (JSONException e) {
+                    call.error(e.getMessage(), e);
+                }
+                break;
+            case Activity.RESULT_CANCELED:
+                call.error("Payment cancelled");
+                break;
+            case AutoResolveHelper.RESULT_ERROR:
+                Status status = AutoResolveHelper.getStatusFromIntent(data);
 
-                        switch (status.getStatusCode()) {
-                            case WalletConstants.ERROR_CODE_DEVELOPER_ERROR: {
-                                call.error(status.getStatusCode() + " ERROR_CODE_DEVELOPER_ERROR");
-                            }
-                            case WalletConstants.ERROR_CODE_BUYER_ACCOUNT_ERROR: {
-                                // https://developers.google.com/android/reference/com/google/android/gms/wallet/WalletConstants
-                                call.error(status.getStatusCode() + " ERROR_CODE_BUYER_ACCOUNT_ERROR");
-                            }
-                            break;
-                            default: {
-                                call.error(Integer.toString(status.getStatusCode()));
-                            }
-                        }
-                        break;
+                switch (status.getStatusCode()) {
+                    case WalletConstants.ERROR_CODE_DEVELOPER_ERROR: {
+                        call.error(status.getStatusCode() + " ERROR_CODE_DEVELOPER_ERROR");
+                    }
+                    case WalletConstants.ERROR_CODE_BUYER_ACCOUNT_ERROR: {
+                        // https://developers.google.com/android/reference/com/google/android/gms/wallet/WalletConstants
+                        call.error(status.getStatusCode() + " ERROR_CODE_BUYER_ACCOUNT_ERROR");
+                    }
+                    break;
+                    default: {
+                        call.error(Integer.toString(status.getStatusCode()));
+                    }
                 }
                 break;
         }
@@ -76,10 +72,13 @@ public class CapacitorGooglePay extends Plugin {
     public void configure(PluginCall call) {
         Boolean useProdEnvironment = call.getBoolean("useProdEnvironment");
 
-        this.paymentsClient = Wallet.getPaymentsClient(
-                getActivity(),
-                new Wallet.WalletOptions.Builder().setEnvironment(useProdEnvironment ? WalletConstants.ENVIRONMENT_PRODUCTION : WalletConstants.ENVIRONMENT_TEST).build()
-        );
+        this.paymentsClient =
+                Wallet.getPaymentsClient(
+                        getActivity(),
+                        new Wallet.WalletOptions.Builder()
+                                .setEnvironment(useProdEnvironment ? WalletConstants.ENVIRONMENT_PRODUCTION : WalletConstants.ENVIRONMENT_TEST)
+                                .build()
+                );
 
         call.success();
     }
@@ -96,7 +95,8 @@ public class CapacitorGooglePay extends Plugin {
         final PluginCall callContext = call;
 
         Task<Boolean> task = this.paymentsClient.isReadyToPay(request);
-        task.addOnCompleteListener(getActivity(),
+        task.addOnCompleteListener(
+                getActivity(),
                 new OnCompleteListener<Boolean>() {
                     @Override
                     public void onComplete(@NonNull Task<Boolean> task) {
@@ -107,7 +107,8 @@ public class CapacitorGooglePay extends Plugin {
                             callContext.error(e.getMessage(), e);
                         }
                     }
-                });
+                }
+        );
     }
 
     @PluginMethod
@@ -120,22 +121,8 @@ public class CapacitorGooglePay extends Plugin {
         PaymentDataRequest request = PaymentDataRequest.fromJson(call.getData().toString());
 
         if (request != null) {
-            final PluginCall callContext = call;
-            final Task<PaymentData> paymentDataTask = this.paymentsClient.loadPaymentData(request);
-
-            paymentDataTask.addOnFailureListener(getActivity(), new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    callContext.error(e.getMessage(), e);
-                }
-            });
-
-            callContext.save();
-            AutoResolveHelper.resolveTask(
-                    paymentDataTask,
-                    getActivity(),
-                    LOAD_PAYMENT_DATA_REQUEST_CODE);
+            this.saveCall(call);
+            AutoResolveHelper.resolveTask(this.paymentsClient.loadPaymentData(request), getActivity(), LOAD_PAYMENT_DATA_REQUEST_CODE);
         }
-
     }
 }
