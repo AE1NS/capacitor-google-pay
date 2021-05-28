4f7851d464be001b3618f32bf15604995148cdbf
Jan Gessinger - A EINS Digital Innovation GmbH <jgessinger@aeins.de>
2021-05-28 15:56:05 +0200

    Capacitor 3 update

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,13 +1,3 @@
-import { WebPlugin } from '@capacitor/core';
-
-export class GooglePayWeb extends WebPlugin {} // Not implemented
-
-declare module '@capacitor/core' {
-  interface PluginRegistry {
-    GooglePay: GooglePayPlugin;
-  }
-}
-
 export interface GooglePayPlugin {
   configure(options: { useProdEnvironment: boolean }): Promise<void>;
   isReadyToPay(isReadyToPayRequest: any): Promise<void>;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 export * from './definitions';
+
+import { registerPlugin } from '@capacitor/core';
+import type { GooglePayPlugin } from './definitions';
+
+const GooglePay = registerPlugin<GooglePayPlugin>('GooglePay', {});
+
+export * from './definitions';
+export { GooglePay };
